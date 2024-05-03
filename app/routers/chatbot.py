1f9357250e5d11e98f6248394e5b40e2c8eb60e2
Ramón Danzos <ramondanzos@outlook.com>
2024-05-02 21:51:44 -0600

    Update chatbot.py

--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -264,7 +264,7 @@ async def create_reservation_bot(res: ReservationBot):
             if len(results) == 0:
                 raise HTTPException(status_code=404, detail="Schedule not found or already occupied")
             schedule_id = results[0][0]
-            return await create_reservation(Reservation(user_id=res.user_id, space_id=res.space_id, schedule_id=int(schedule_id), user_requirements= result))
+            return await create_reservation(Reservation(user_id=res.user_id, space_id=res.space_id, schedule_id=int(schedule_id), user_requirements= "1=1,2=3,4=6"))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
