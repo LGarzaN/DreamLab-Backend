ed4d4e4dbdb712f9f025d23d3219a77258b85c39
Luis Eduardo Garza Naranjo <luis.garzanaranjo@gmail.com>
2024-06-11 11:47:17 -0600

    reservation confirmation change

--- app/functions.py
+++ app/functions.py
@@ -19,36 +19,28 @@ async def create_confirmed_reservation(res: Reservation):
     try:
         async with DB() as db:
             group_code = str(uuid4())[:7]
-            query ='''
+            query =f'''
                 DECLARE @GroupID INT;
                 INSERT INTO [dbo].[ReservationGroup] (GroupCode)
                 OUTPUT INSERTED.GroupID
-                VALUES (?);
+                VALUES ('{group_code}');
 
                 SET @GroupID = SCOPE_IDENTITY(); 
 
                 INSERT INTO [dbo].[Reservation] (GroupId, UserId, SpaceId, ScheduleId, UserRequirements) 
-                VALUES (@GroupID, ?, ?, ?, ?);
+                VALUES (@GroupID, {res.user_id}, {res.space_id}, {res.schedule_id}, '{res.user_requirements}');
 
-                UPDATE [dbo].[Schedule] SET Occupied = 1 WHERE ScheduleId = ?;
+                UPDATE [dbo].[Schedule] SET Occupied = 1 WHERE ScheduleId = {res.schedule_id};
 
-                MERGE INTO [dbo].[Statistic] AS target
-                USING (SELECT ? AS UserId) AS source (UserId)
-                ON target.UserId = source.UserId
-                WHEN MATCHED THEN
-                    UPDATE SET Reservations = target.Reservations + 1,
-                            StudyHours = target.StudyHours + 1
-                WHEN NOT MATCHED THEN
-                    INSERT (UserId, Reservations, StudyHours)
-                    VALUES (source.UserId, 1, 1);
+                UPDATE [dbo].[Statistic] SET Reservations = Reservations + 1, StudyHours = StudyHours + 1 WHERE UserId = {res.user_id};
                 
                 INSERT INTO [dbo].[UserRequirements] (GroupId, RequirementId, Quantity) VALUES (@GroupID, 1, 1);
               '''
             # if len(res.user_requirements) > 0:
             #     query += get_requirements_query(res.user_requirements)
             # print(query)
-            params = (group_code, res.user_id, res.space_id, res.schedule_id, res.schedule_id, res.user_requirements, res.user_id)
-            results = await db.execute_query_insert(query=query, params=params)
+            #params = (group_code, res.user_id, res.space_id, res.schedule_id, res.user_requirements, res.schedule_id, res.user_id)
+            results = await db.execute_query_insert(query=query)
             return {"message": "Reservation created successfully", "results": results}
     except Exception as e:
         print(e)
